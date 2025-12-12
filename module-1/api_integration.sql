882517c1abe8003b9fa79bed57c23c8ada9a3656
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2025-12-12 13:24:37 +0500

    feat: Add Snowflake SQL script to create Git API integration, secret, and repository object.

--- module-1/api_integration.sql
+++ module-1/api_integration.sql
@@ -22,4 +22,31 @@ CREATE OR REPLACE GIT REPOSITORY course_repo.public.advanced_data_engineering_sn
   GIT_CREDENTIALS = ;
 
 -- List the git repositories
+SHOW GIT REPOSITORIES;USE ROLE accountadmin;
+CREATE DATABASE course_repo;
+USE SCHEMA public;
+
+-- Create credentials
+CREATE OR REPLACE SECRET course_repo.public.github_pat
+  TYPE = password
+  USERNAME = 'asadali27232'
+  PASSWORD = '';
+
+SHOW SECRETS;
+
+-- Create the API integration
+CREATE OR REPLACE API INTEGRATION git_api_integration
+  API_PROVIDER = git_https_api
+  API_ALLOWED_PREFIXES = ('https://github.com/asadali27232') -- URL to your GitHub profile
+  ALLOWED_AUTHENTICATION_SECRETS = (course_repo.public.github_pat)
+  ENABLED = TRUE;
+  
+
+-- Create the git repository object
+CREATE OR REPLACE GIT REPOSITORY course_repo.public.advanced_data_engineering_snowflake
+  API_INTEGRATION = git_api_integration -- Name of the API integration defined above
+  ORIGIN = 'https://github.com/asadali27232/advanced-data-engineering-snowflake.git' -- Insert URL of forked repo
+  GIT_CREDENTIALS = course_repo.public.github_pat;
+
+-- List the git repositories
 SHOW GIT REPOSITORIES;
